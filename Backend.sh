4c88717e6d277b5dbea67926702a084dc37bf560
Bhavya Sri P <pallembhavya1@gmail.com>
2024-04-25 16:31:00 +0530

    Common scripts usage

--- Backend.sh
+++ Backend.sh
@@ -1,34 +1,8 @@
 #!/bin/bash
 
-user=$(id -u)
-timestamp=$(date +%F-%H-%M-%S)
-scriptname=$( echo $0 | cut -d "." -f1)
-logfile=/tmp/$scriptname-$timestamp.log
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+source ./Common.sh
 
-echo "Pls enter DB pswwd"
-read -s mysql_root_password
-
-validate(){
-    if [ $1 -eq 0 ]
-    then 
-        echo -e "$2...$G Success $N"
-    else
-        echo -e "$2...$R Failure $N"
-    fi 
-}
-
-if [ $user -eq 0 ]
-then    
-    echo -e "user is having $G root access $N" 
-else
-    echo -e " $R Run with root access $N"
-    exit 1
-fi
-
+current_root
 
 dnf module disable nodejs -y &>>$logfile
 validate $? "Disabling nodejs"
