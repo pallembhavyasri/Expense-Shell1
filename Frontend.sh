4389b06b0a64a84698d2af7991e2faec43cc58d3
Bhavya Sri P <pallembhavya1@gmail.com>
2024-04-25 19:33:58 +0530

    Common scripts usage

--- Frontend.sh
+++ Frontend.sh
@@ -1,30 +1,8 @@
 #!/bin/bash
 
-user=$(id -u)
-timestamp=$(date +%F-%H-%M-%S)
-scriptname=$( echo $0 | cut -d "." -f1)
-logfile=/tmp/$scriptname-$timestamp.log
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+source ./Common.sh 
 
-validate(){
-    if [ $1 -eq 0 ]
-    then 
-        echo -e "$2...$G Success $N"
-    else
-        echo -e "$2...$R Failure $N"
-    fi 
-}
-
-if [ $user -eq 0 ]
-then    
-    echo -e "user is having $G root access $N" 
-else
-    echo -e " $R Run with root access $N"
-    exit 1
-fi
+current_root
 
 dnf install nginx -y &>>$logfile
 validate $? "Installing Nginx"
@@ -45,7 +23,7 @@ cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>$logfile #here unzip is the idempotent because in the above step we are delecting everytime
 validate $? "Unziping the file"
 
-cp /home/ec2-user/Expense-Shell/Frontend.Service /etc/nginx/default.d/expense.conf &>>$logfile
+cp /home/ec2-user/Expense-Shell1/Frontend.Service /etc/nginx/default.d/expense.conf &>>$logfile
 validate $? "copied the frontend service"
 
 systemctl restart nginx &>>$logfile
